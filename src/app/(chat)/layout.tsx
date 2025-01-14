42bd652a08767532b8b43110cca2d13083f51087
Naveen Singh <io.naveens@gmail.com>
2025-01-14 15:46:04 +0700

    v 0.1 dev

--- src/app/(chat)/layout.tsx
+++ src/app/(chat)/layout.tsx
@@ -7,8 +7,8 @@ import { Toaster } from "@/components/ui/sonner";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Ollama UI",
-  description: "Ollama chatbot web interface",
+  title: "FarmGPT-Powered by PhyFarm",
+  description: "AI assistant for farming",
 };
 
 export const viewport = {
